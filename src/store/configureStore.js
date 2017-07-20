0f964f30999f5fba6effd34fe7939bb62996212a
Michael Sund <michael@osund.com>
2017-07-20 18:32:48 +0200

    persistent state from redux to localStorage done.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,7 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"optionalDependencies": false}] */
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import persistState from 'redux-localstorage';
 import devTools from 'remote-redux-devtools';
 import rootReducer from '../reducers';
 
@@ -9,6 +10,7 @@ const configureStore = (initialState) => {
   console.log(`local NODE_ENV is ${process.env.NODE_ENV}`);
   const middleware = compose(
     applyMiddleware(thunk),
+    persistState(),
     process.env.NODE_ENV === 'development' ? devTools({ realtime: true, port: 8000 }) : f => f
   );
 
